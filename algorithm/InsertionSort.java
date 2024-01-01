792eac333f0d94243d05b2b4232f610aa7f9e5ca
BSMITSME <sun4461637@gmail.com>
2024-01-01 20:59:35 +0900

    add package named document

---
+++ algorithm/InsertionSort.java
@@ -0,0 +1,18 @@
+package algorithm;
+public class InsertionSort {
+    public static void main(String[] args) {
+        int i, j, temp; 
+        int[] array = {1, 10, 3, 2 ,4 ,5, 9, 7, 6, 8};
+        for(i=0; i<9; i++){
+            j = i;
+            while(array[j] > array[j+1]){
+                temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+        for(i = 0; i<10; i++){
+            System.out.printf("%d ", array[i]);
+        }
+    }
+}
---
+++ document/ArrayCopyOfDemo.java
@@ -0,0 +1,17 @@
+package document;
+
+class ArrayCopyOfDemo {
+    public static void main(String[] args) {
+        String[] copyFrom = {
+            "Affogato", "Americano", "Cappuccino", "Corretto", "Cortado",
+            "Doppio", "Espresso", "Frappucino", "Freddo", "Lungo", "Macchiato",
+            "Marocchino", "Ristretto" };
+        var messsage = "hello";
+        
+
+        String[] copyTo = java.util.Arrays.copyOfRange(copyFrom, 2, 9);
+        for (String coffee : copyTo) {
+            System.out.print(coffee + " ");
+        }
+    }
+}
