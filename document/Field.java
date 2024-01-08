d203557f7c3ccf459704a2b0b6acc020cd64d089
BSMITSME <sun4461637@gmail.com>
2024-01-08 15:38:22 +0900

    about var

--- document/Field.java
+++ document/Field.java
@@ -1,6 +1,8 @@
 package document;
 
-import java.lang.instrument.ClassFileTransformer;
+// 클래스 변수 : 클래스가 메모리에 올라갈 때 생성, 프로그램이 종료될 떄 종료, 메소드 영역에 위치 (초기화를 하지 않아도 변수의 타입에 맞게 자동 초기화)
+// 인스턴스 변수 : 인스턴스 생성 시 생성, 인스턴스가 종료되면 종료, 힙 영역에 위치 (초기화를 하지 않아도 변수의 타입에 맞게 자동 초기화)
+// 지역 변수 : 블록 내에서 변수의 선언문이 실행될 때 생성, 블록을 벗어날 때 종료, 스택에 위치
 
 class ClassField{
     static int classVar = 10; // 클래스 변수 선언
